84fee98cbe0f89c8e3fb33f4bb3642ed91f9ebc5
He <18735526@qq.com>
2019-06-18 19:57:01 +0800

    '任务9：反射（Reflection）'

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Query, Headers, Param, Body, Post, HttpException, HttpStatus, ForbiddenException, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Query, Headers, Param, Body, Post, HttpException, HttpStatus, ForbiddenException, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, SetMetadata } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
 import { DemoService } from './providers/demo/demo.service';
 import { DemoFilter } from '../../core/filters/demo.filter';
@@ -28,6 +28,7 @@ export class PostsController {
     @Post()
     //@UseFilters(DemoFilter)
     @UsePipes(ValidationPipe)
+    @SetMetadata('roles', ['member'])
     Store(@Body() post: CreatePostDto){
         // throw new ForbiddenException('没有权限！');
         this.demoService.create(post);
