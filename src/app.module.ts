652e472686420963570647ec667fa73d26a85315
He <18735526@qq.com>
2019-06-19 19:39:33 +0800

    '任务3：绑定拦截器（全局范围）'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
 import { DemoMiddleware } from './core/middlewares/demo.middleware';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { DemoRolseGuard } from './core/guards/demo-rolse.guard';
+import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 
 @Module({
   imports: [PostsModule],
@@ -14,6 +15,10 @@ import { DemoRolseGuard } from './core/guards/demo-rolse.guard';
     {
       provide: APP_GUARD,
       useClass: DemoRolseGuard
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor
     }
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DemoFilter } from './core/filters/demo.filter';
+import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
