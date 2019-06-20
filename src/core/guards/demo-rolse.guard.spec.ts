0def6b9ca3788491505f67cecb0a71587d5efd32
He <18735526@qq.com>
2019-06-20 19:25:17 +0800

    '任务1：准备项目（全新项目）'

--- src/core/guards/demo-rolse.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DemoRolseGuard } from './demo-rolse.guard';
-
-describe('DemoRolseGuard', () => {
-  it('should be defined', () => {
-    expect(new DemoRolseGuard()).toBeDefined();
-  });
-});
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DemoFilter } from './core/filters/demo.filter';
-import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new DemoFilter());
   await app.listen(3000);
 }
 bootstrap();
