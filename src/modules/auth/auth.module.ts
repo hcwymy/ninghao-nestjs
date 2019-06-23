f2d04b215f19c070b993005be988f905e5731b84
He <18735526@qq.com>
2019-06-23 17:34:50 +0800

    '任务6：签发JWT（jwtService.sign）'

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: 'Ll7nO1oxx9BMqoA4HRuSFQUeMpk6sx45',
+      signOptions: {
+        expiresIn: '12h'
+      }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
