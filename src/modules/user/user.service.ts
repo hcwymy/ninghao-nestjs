07d1977d22ffdfc396f825c572e9dbe2a22b2a3b
He <18735526@qq.com>
2019-06-23 14:29:37 +0800

    '任务6：自定义创建用户时用户已存在的异常'

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { UserDto } from './user.dto';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class UserService {
@@ -12,6 +13,13 @@ export class UserService {
     ){}
 
     async store(data: UserDto){
+        const {name} = data;
+        const user = await this.userRepository.findOne({ name });
+
+        if(user){
+            throw new BadRequestException('用户名已存在')
+        }
+
         const entity = await this.userRepository.create(data);
         await this.userRepository.save(entity);
         return entity;
