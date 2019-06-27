d979340f2876fee650392f545130955ca153951f
He <18735526@qq.com>
2019-06-27 22:34:20 +0800

    '任务4：定义评论与其它实体的关系'

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryColumn, PrimaryGeneratedColumn, Column, CreateDateColumn
 import { Exclude } from 'class-transformer'
 import * as bcrypt from 'bcryptjs'
 import { Post } from "../post/post.entity";
+import { Comment } from "../comment/comment.entity";
 
 @Entity()
 export class User {
@@ -27,6 +28,9 @@ export class User {
     @ManyToMany(type => Post, post => post.liked)
     @JoinTable()
     voted: Post[];
+
+    @OneToMany(type => Comment, comment => comment.user)
+    comments: Comment[];
 
     @BeforeInsert()
     @BeforeUpdate()
